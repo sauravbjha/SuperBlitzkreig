c981b4b49566b373b28499a899f64ec2baf17e28
arindamdawn <arindamdawn3@gmail.com>
2020-08-29 16:15:37 +0530

    updated blog content

--- components/alert.js
+++ components/alert.js
@@ -1,6 +1,6 @@
-import Container from './container'
-import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
+import Container from './container';
+import cn from 'classnames';
+import { EXAMPLE_PATH } from '../lib/constants';
 
 export default function Alert({ preview }) {
   return (
@@ -11,32 +11,8 @@ export default function Alert({ preview }) {
       })}
     >
       <Container>
-        <div className="py-2 text-center text-sm">
-          {preview ? (
-            <>
-              This is page is a preview.{' '}
-              <a
-                href="/api/exit-preview"
-                className="underline hover:text-cyan duration-200 transition-colors"
-              >
-                Click here
-              </a>{' '}
-              to exit preview mode.
-            </>
-          ) : (
-            <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
-        </div>
+        <div className="py-2 text-center text-sm"></div>
       </Container>
     </div>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
+import Container from '../components/container';
+import MoreStories from '../components/more-stories';
+import HeroPost from '../components/hero-post';
+import Intro from '../components/intro';
+import Layout from '../components/layout';
+import { getAllPostsForHome } from '../lib/api';
+import Head from 'next/head';
+import { CMS_NAME } from '../lib/constants';
 
 export default function Index({ preview, allPosts }) {
-  const heroPost = allPosts[0].node
-  const morePosts = allPosts.slice(1)
+  const heroPost = allPosts[0].node;
+  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Saurav Jha's Blog</title>
         </Head>
         <Container>
           <Intro />
@@ -32,12 +32,12 @@ export default function Index({ preview, allPosts }) {
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps({ preview = false, previewData }) {
-  const allPosts = await getAllPostsForHome(previewData)
+  const allPosts = await getAllPostsForHome(previewData);
   return {
     props: { preview, allPosts },
-  }
+  };
 }
